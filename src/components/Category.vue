3e796c30d24817ddf9e4d79538fe7175e4bbe6c2
tauhidbs23 <tauhidul.islam@brainstation-23.com>
2021-09-16 17:49:48 +0600

    structure done

---
+++ src/components/Category.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div class="header">
+      <div class="header">All Category</div>
+      <div class="create-category-btn">CREATE CATEGORY</div>
+    </div>
+    <div class="content">
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>iojsd</td>
+            <td>pencil and delete icon</td>
+          </tr>
+          <tr>
+            <td>iojsd</td>
+            <td>pencil and delete icon</td>
+          </tr>
+          <tr>
+            <td>iojsd</td>
+            <td>pencil and delete icon</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Category",
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/Post.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div class="header">
+      <div class="title">All Post</div>
+      <div class="creat-post">CREATE POST</div>
+    </div>
+    <div class="content">
+      <table>
+        <thead>
+          <th>Title</th>
+          <th>Action</th>
+        </thead>
+        <tbody>
+          <tr>
+            <td>intro to css</td>
+            <td>up pencil and delete icon</td>
+          </tr>
+          <tr>
+            <td>intro to css</td>
+            <td>up pencil and delete icon</td>
+          </tr>
+          <tr>
+            <td>intro to css</td>
+            <td>up pencil and delete icon</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Post",
+};
+</script>
+
+<style>
+</style>
